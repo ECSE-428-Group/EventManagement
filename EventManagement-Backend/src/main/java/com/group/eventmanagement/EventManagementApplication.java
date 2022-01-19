b33a6d9bba02de6af72812b70caef20c9c044381
Zhanna <zklimanova@yahoo.com>
2022-01-18 23:15:33 -0500

    Initializing project.

---
+++ EventManagement-Backend/src/main/java/com/group/eventmanagement/EventManagementApplication.java
@@ -0,0 +1,21 @@
+package com.group.eventmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@SpringBootApplication
+public class EventManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventManagementApplication.class, args);
+	}
+	
+	@RequestMapping("/")
+	public String greeting(){
+		return "Hello world!";
+	}
+
+}
