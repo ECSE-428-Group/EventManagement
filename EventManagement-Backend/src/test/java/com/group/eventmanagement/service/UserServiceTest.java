988f09d4c17420403c90c2f5bec658b779b80f59
theSaab <saba.fathi@emailgmail.info>
2022-02-10 20:46:31 -0500

    pushin p, mergin

--- EventManagement-Backend/src/test/java/com/group/eventmanagement/service/UserServiceTest.java
+++ EventManagement-Backend/src/test/java/com/group/eventmanagement/service/UserServiceTest.java
@@ -18,26 +18,26 @@ import com.group.eventmanagement.repository.UserRepository;
 
 @ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
-	
+
 	@Mock
 	private UserRepository userRepository;
-	
+
 	@InjectMocks
 	private UserService userService;
-	
+
 	@Test
 	public void testCreateValidUser() {
 		// Whenever a user is saved, return nothing
 		when(userRepository.save(any(User.class))).thenReturn(null);
-		
+
 		User newlyCreatedUser = null;
 		try {
-			newlyCreatedUser = 
+			newlyCreatedUser =
 					userService.createUser(TestData.userUsername, TestData.userPassword, TestData.userFirstname, TestData.userLastname, TestData.userBirthday, TestData.userEmail);
 		} catch (IllegalArgumentException e) {
 			fail();
 		}
-		
+
 		assertNotNull(newlyCreatedUser);
 		assertEquals(TestData.userUsername, newlyCreatedUser.getUsername());
 		assertEquals(TestData.userFirstname, newlyCreatedUser.getFirstName());
@@ -46,50 +46,50 @@ public class UserServiceTest {
 		assertEquals(TestData.userBirthday, newlyCreatedUser.getBirthday());
 		assertEquals(TestData.userPassword, newlyCreatedUser.getPassword());
 	}
-	
+
 	@Test
 	public void testCreateInvalidUser() {
 		User invalidUser = null;
 		String error = "";
-		
+
 		try {
 			invalidUser = userService.createUser(" ", "", "", "", null, "");
 		} catch (IllegalArgumentException e) {
 			error = e.getMessage();
 		}
-		
+
 		assertNull(invalidUser);
 		assertEquals("Username cannot be empty! Password cannot be empty! First name cannot be empty. "
 				+ "Last name cannot be empty. Date of birth is incorrect. Email is incorrect.", error);
 	}
-	
+
 	@Test
 	public void testCreateUserWithIncorrectEmailFormat() {
 		User user = null;
 		String error = "";
-		
+
 		try {
 			user = userService.createUser(TestData.userUsername, TestData.userPassword, TestData.userFirstname, TestData.userLastname, TestData.userBirthday, TestData.invalidUserEmail);
 		} catch (IllegalArgumentException e) {
 			error = e.getMessage();
 		}
-		
+
 		assertNull(user);
 		assertEquals("Email is incorrect.", error);
 	}
-	
+
 	@Test
 	public void testCreateDuplicateUsernameUser() {
 		when(userRepository.existsByUsername(TestData.existentUsername)).thenReturn(true);
 		User user = null;
 		String error = "";
-		
+
 		try {
 			user = userService.createUser(TestData.existentUsername, TestData.userPassword, TestData.userFirstname, TestData.userLastname, TestData.userBirthday, TestData.userEmail);
 		} catch (IllegalArgumentException e) {
 			error = e.getMessage();
 		}
-		
+
 		assertNull(user);
 		assertEquals("This username already exists!", error);
 	}
