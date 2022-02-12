9114aad0ec4f730dab664e86d74e71a9a226aee0
theSaab <saba.fathi@emailgmail.info>
2022-02-12 14:00:18 -0500

    add service controller files

---
+++ EventManagement-Backend/src/main/java/com/group/eventmanagement/controller/CommentController.java
@@ -0,0 +1,10 @@
+package com.group.eventmanagement.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*")
+@RestController
+public class CommentController {
+
+}
---
+++ EventManagement-Backend/src/main/java/com/group/eventmanagement/service/EventService.java
@@ -0,0 +1,8 @@
+package com.group.eventmanagement.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EventService {
+
+}
