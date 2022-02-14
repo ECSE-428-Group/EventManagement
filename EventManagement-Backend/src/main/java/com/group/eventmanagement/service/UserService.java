977117ba1091c4c275e0d56cdb451320419a5d2e
cWetaski <charles.wetaski@mail.mcgill.ca>
2022-02-14 18:58:36 -0500

    Added updateUser method to user service class

--- EventManagement-Backend/src/main/java/com/group/eventmanagement/service/UserService.java
+++ EventManagement-Backend/src/main/java/com/group/eventmanagement/service/UserService.java
@@ -65,5 +65,77 @@ public class UserService {
 
 		return newUser;
 	}
-
+	
+	///// UPDATE OF USER /////
+	@Transactional
+	public User updateUser(String username, String newPassword, String newFirstName, String newLastName, Timestamp newBirthday, String newEmail) {
+		String error = "";
+		
+		if(!userRepository.existsByUsername(username)) {
+			error += "User does not exist";
+		}
+		
+		if (newBirthday.after(new Timestamp(System.currentTimeMillis()))) {
+			error += "Invalid birthday";
+		}
+		
+		error = error.trim();
+		if (error.length() > 0) {
+			throw new IllegalArgumentException(error);
+		}
+		
+		User user = userRepository.findUserByUsername(username);
+		
+		if(newPassword == null) {
+			newPassword = user.getPassword();
+		}
+		
+		if(newFirstName == null) {
+			newFirstName = user.getFirstName();
+		}
+		
+		if(newLastName == null) {
+			newLastName = user.getLastName();
+		}
+		
+		if(newBirthday == null) {
+			newBirthday = user.getBirthday();
+		}
+		
+		if(newEmail == null) {
+			newEmail = user.getEmail();
+		}
+		
+		if(newPassword.trim().length() == 0) {
+			error += "Password cannot be empty string";
+		}
+		
+		if(newFirstName.trim().length() == 0) {
+			error += "First name cannot be empty string";
+		}
+		
+		if(newLastName.trim().length() == 0) {
+			error += "Last name cannot be empty string";
+		}
+		
+		if(newEmail.trim().length() == 0) {
+			error += "Email cannot be empty string";
+		}
+		
+		error = error.trim();
+		if (error.length() > 0) {
+			throw new IllegalArgumentException(error);
+		}
+		
+		
+		user.setPassword(newPassword);
+		user.setFirstName(newFirstName);
+		user.setLastName(newLastName);
+		user.setBirthday(newBirthday);
+		user.setEmail(newEmail);
+		
+		userRepository.save(user);
+		
+		return user;
+	}
 }
