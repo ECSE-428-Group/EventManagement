b80a58979dd05ecbb5dd6e2b1b4b4486c61aec74
ziadjbr <77809437+ziadjbr@users.noreply.github.com>
2022-02-16 12:42:12 -0500

    Added get and remove methods to controller

--- EventManagement-Backend/src/main/java/com/group/eventmanagement/controller/EventController.java
+++ EventManagement-Backend/src/main/java/com/group/eventmanagement/controller/EventController.java
@@ -1,10 +1,77 @@
 package com.group.eventmanagement.controller;
 
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.group.eventmanagement.service.EventService;
+import com.group.eventmanagement.model.Event;
+import com.group.eventmanagement.model.Tag;
+import com.group.eventmanagement.model.User;
+import com.group.eventmanagement.model.Post;
+import com.group.eventmanagement.repository.EventRepository;
 
 @CrossOrigin(origins = "*")
 @RestController
 public class EventController {
 
+	
+	private EventService eventService;
+	
+	
+	@Autowired
+	public EventRepository repo;
+	
+	@Autowired
+	public EventController(EventService eventService) {
+		this.eventService = eventService;
+	}
+	
+	@GetMapping(value = "/event/{eventID}")
+	public User getAttendee(@PathVariable ("eventID") Long id,
+	    		@RequestParam String callerUsername,
+	    		@RequestParam String attendeeUsername) throws IllegalArgumentException{
+		Event event = repo.findByEventId(id);
+		User attendee = eventService.getAttendee(callerUsername, id, attendeeUsername);
+		return attendee;
+	}
+	
+	@GetMapping(value = "/event/{eventID}")
+	public List<User> getAllAttendees(@PathVariable ("eventID") Long id,
+	    		@RequestParam String callerUsername
+	    		) throws IllegalArgumentException{
+		Event event = repo.findByEventId(id);
+		List<User> attendees = eventService.getAllAttendees(callerUsername, id);
+		return attendees;
+	}
+	
+	@PutMapping(value = "/event/{eventID}")
+	public Event removeAttendee(@PathVariable ("eventID") Long id,
+			@RequestParam String callerUsername, @RequestParam String attendeeUsername) throws IllegalArgumentException{
+		Event event = repo.findByEventId(id);
+		 eventService.removeAttendeeFromEvent(callerUsername, id, attendeeUsername);		 
+		 return event;
+	 }
+	 
+	 @PutMapping(value = "/event/{eventID}")
+	    public Event removeAllAttendees(@PathVariable ("eventID") Long id,
+	    		@RequestParam String callerUsername
+	    		) throws IllegalArgumentException{
+		 Event event = repo.findByEventId(id);
+		 eventService.removeAllAttendeesFromEvent(callerUsername, id);		 
+		 return event;
+	 }
+	
+	
 }
