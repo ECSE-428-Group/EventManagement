a2cd5162b5d2d8318bea62f83475ea2bfffb626f
André Wakil <andre.wakil@mail.mcgill.ca>
2022-02-17 17:55:37 -0500

    Initial commit

--- EventManagement-Backend/src/main/java/com/group/eventmanagement/controller/CommentController.java
+++ EventManagement-Backend/src/main/java/com/group/eventmanagement/controller/CommentController.java
@@ -1,10 +1,37 @@
 package com.group.eventmanagement.controller;
 
+import com.group.eventmanagement.model.User;
+import com.group.eventmanagement.service.CommentService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins = "*")
 @RestController
 public class CommentController {
 
+    private CommentService commentService;
+
+    @Autowired
+    public CommentController(CommentService commentService) {
+        this.commentService = commentService;
+    }
+
+    @PostMapping(value= {
+        "/posts/{postId}/comments",
+        "/posts/{postId}/comments/"
+    })
+    public Comment createComment(
+        @RequestParam(name="content")String content, 
+        @RequestParam(name="commenter")User commenter
+        ) throws IllegalArgumentException{
+            // TODO: Get post
+            Post post = null;
+            // TODO: Ask how authorization works
+            Comment newComment = commentService.createComment(content, post, commenter);
+            return newComment;
+    }
 }
--- EventManagement-Backend/src/main/java/com/group/eventmanagement/service/CommentService.java
+++ EventManagement-Backend/src/main/java/com/group/eventmanagement/service/CommentService.java
@@ -1,8 +1,53 @@
 package com.group.eventmanagement.service;
 
+import javax.transaction.Transactional;
+
+import com.group.eventmanagement.repository.CommentRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.group.eventmanagement.model.Comment;
+import com.group.eventmanagement.model.Post;
+import com.group.eventmanagement.model.User;
 
 @Service
 public class CommentService {
 
+    CommentRepository commentRepository;
+
+    @Autowired
+    public CommentService(CommentRepository commentRepository) {
+        this.commentRepository = commentRepository;
+    }
+
+    @Transactional
+    public Comment createComment(String content, Post post, User commenter) {
+        String error = "";
+
+        // Input validation
+        if(content == null || content.trim().length() <= 0) {
+            error += "Comment cannot be empty! ";
+        }
+        if(post == null) {
+            error += "Comment must be linked to a post! ";
+        }
+        if(commenter == null) {
+            error += "Comment must be linked to a user! ";
+        }
+
+        error = error.trim();
+		if(error.length() > 0) {
+			throw new IllegalArgumentException(error);
+		}
+
+        Comment newComment = new Comment();
+        newComment.setContent(content);
+        newComment.setPost(post);
+        newComment.setCommenter(commenter);
+
+        commentRepository.save(newComment);
+        
+        return newComment;
+    }
 }
