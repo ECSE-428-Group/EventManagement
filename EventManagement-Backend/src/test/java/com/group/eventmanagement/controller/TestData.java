7807407b2bcdb0d248467abbae27262aa744d82a
theSaab <saba.fathi@emailgmail.info>
2022-02-20 14:43:02 -0500

    attempt start merge

--- EventManagement-Backend/src/test/java/com/group/eventmanagement/controller/TestData.java
+++ EventManagement-Backend/src/test/java/com/group/eventmanagement/controller/TestData.java
@@ -7,12 +7,12 @@ import com.group.eventmanagement.model.Admin;
 import com.group.eventmanagement.model.User;
 
 public class TestData {
-	
+
 	/////////////////////////// SERVER /////////////////////////////////
-	
+
 	static String SERVER_URL = "https://event-management-app-backend.herokuapp.com/";
 
-	
+
 	/////////////////////////// USERS /////////////////////////////////
 
 	// USER CONSTANTS
@@ -22,13 +22,13 @@ public class TestData {
 	static String userEmail = "test@email.com";
 	static String userPassword = "test1234";
 	static Timestamp userBirthday = new Timestamp(System.currentTimeMillis());
-	
+
 	static LocalDate userBirthday2 = LocalDate.of(1990, 1, 8);
 	static Timestamp userBirthday2Con = Timestamp.valueOf(userBirthday2.atStartOfDay());
-	
+
 	// INVALID CONSTANTS
 	static String invalidUserUsername = "invalidUser";
-	
+
 	public static User createUserObject(String username, String password, String firstName, String lastName,
 			Timestamp birthday, String email) {
 		User user = new User();
@@ -38,23 +38,23 @@ public class TestData {
 		user.setLastName(lastName);
 		user.setPassword(password);
 		user.setEmail(email);
-		
+
 		return user;
 	}
-	
-	
+
+
 	/////////////////////////// ADMINS /////////////////////////////////
-	
+
 	// ADMIN CONSTANTS
 	static String adminUsername = "admin";
 	static String adminFirstname = "Main";
 	static String adminLastname = "Admin";
 	static String adminEmail = "admin@mail.com";
 	static String adminPassword = "adminpassword";
-	
+
 	// INVALID CONSTANTS
 	static String invalidAdminUsername = "invalid";
-	
+
 	public static Admin createAdminObject(String username, String password, String firstName, String lastName,
 			String email) {
 		Admin admin = new Admin();
@@ -63,7 +63,7 @@ public class TestData {
 		admin.setLastName(lastName);
 		admin.setPassword(password);
 		admin.setEmail(email);
-		
+
 		return admin;
 	}
 }
