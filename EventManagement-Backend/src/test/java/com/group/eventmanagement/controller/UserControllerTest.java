3875bd0eb205468e279b836d46c7c22dcf296b89
cWetaski <charles.wetaski@mail.mcgill.ca>
2022-02-22 23:22:51 -0500

    Added updateUser Controller Tests

--- EventManagement-Backend/src/test/java/com/group/eventmanagement/controller/UserControllerTest.java
+++ EventManagement-Backend/src/test/java/com/group/eventmanagement/controller/UserControllerTest.java
@@ -3,6 +3,7 @@ package com.group.eventmanagement.controller;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -59,4 +60,42 @@ public class UserControllerTest {
 		}
 	}
 	
+	@Test
+	public void testUserUpdate() {
+		User mockUser = TestData.createUserObject(TestData.userUsername, TestData.userPassword, TestData.userFirstname, TestData.userLastname, TestData.userBirthday2Con, TestData.userEmail);
+		when(userService.updateUser(TestData.userUsername, TestData.userPassword, TestData.userPassword, TestData.userFirstname, TestData.userLastname, TestData.userBirthday2Con, TestData.userEmail))
+		.thenReturn(mockUser);
+		
+		try {
+			this.mockMvc.perform(put("/userprofile/"+TestData.userUsername)
+					.param("curPassword", TestData.userPassword)
+					.param("newPassword", TestData.userPassword)	
+					.param("firstName", TestData.userFirstname)
+					.param("lastName", TestData.userLastname)
+					.param("birthday", TestData.userBirthday2.toString())
+					.param("email", TestData.userEmail)
+					).andExpect(status().isOk());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testUserUpdateInvalid() {
+		when(userService.updateUser(TestData.userUsername, TestData.userPassword, TestData.invalidUserPassword, TestData.userFirstname, TestData.userLastname, TestData.userBirthday2Con, TestData.userEmail))
+		.thenThrow(IllegalArgumentException.class);
+		
+		try {
+			this.mockMvc.perform(put("/userprofile/"+TestData.userUsername)
+					.param("curPassword", TestData.userPassword)
+					.param("newPassword", TestData.invalidUserPassword)	
+					.param("firstName", TestData.userFirstname)
+					.param("lastName", TestData.userLastname)
+					.param("birthday", TestData.userBirthday2.toString())
+					.param("email", TestData.userEmail)
+					).andExpect(status().isInternalServerError());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 } 
