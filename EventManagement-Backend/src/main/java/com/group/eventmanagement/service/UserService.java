9f4478209fa32a33d9e948ad53e85132e3898cc4
theSaab <saba.fathi@emailgmail.info>
2022-03-17 14:37:49 -0400

    new user endpoint

--- EventManagement-Backend/src/main/java/com/group/eventmanagement/service/UserService.java
+++ EventManagement-Backend/src/main/java/com/group/eventmanagement/service/UserService.java
@@ -1,6 +1,9 @@
 package com.group.eventmanagement.service;
 
 import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
 import jakarta.mail.internet.AddressException;
 import jakarta.mail.internet.InternetAddress;
 
@@ -30,7 +33,7 @@ public class UserService {
 		if(userRepository.existsByUsername(username)) {
 			error += "This username already exists! ";
 		}
-		
+
 		if(!validateUsername(username)) {
 			error += "Username is invalid. ";
 		}
@@ -38,7 +41,7 @@ public class UserService {
 		if(!validatePassword(password)) {
 			error += "Password is invalid. ";
 		}
-		
+
 		if(firstName == null || firstName.trim().length() <= 0) {
 			error += "First name cannot be empty. ";
 		}
@@ -72,7 +75,7 @@ public class UserService {
 
 		return newUser;
 	}
-	
+
 	///// UPDATE OF USER /////
 	@Transactional
 	public User updateUser(String username, String curPassword, String newPassword, String newFirstName, String newLastName, Timestamp newBirthday, String newEmail) {
@@ -81,74 +84,74 @@ public class UserService {
 		if(!userRepository.existsByUsername(username)) {
 			error += "User does not exist. ";
 		}
-		
+
 		error = error.trim();
 		if (error.length() > 0) {
 			throw new IllegalArgumentException(error);
 		}
-		
+
 		User user = userRepository.findUserByUsername(username);
-		
+
 		if(!curPassword.equals(user.getPassword())) {
 			error += "Incorrect Password. ";
 		}
-		
+
 		if(newPassword == null || newPassword.length() == 0) {
 			newPassword = user.getPassword();
 		}
-		
+
 		if(newFirstName == null || newFirstName.trim().length() == 0) {
 			newFirstName = user.getFirstName();
 		}
-		
+
 		if(newLastName == null || newLastName.trim().length() == 0) {
 			newLastName = user.getLastName();
 		}
-		
+
 		if(!validatePassword(newPassword)) {
 			error += "Password is invalid. ";
 		}
-		
+
 		if(newBirthday == null) {
 			newBirthday = user.getBirthday();
 		} else if (newBirthday.after(new Timestamp(System.currentTimeMillis()))) {
-			error += "Birthday is invalid. ";	
+			error += "Birthday is invalid. ";
 		}
-		
+
 		if(newEmail == null) {
 			newEmail = user.getEmail();
 		}
-		
+
 		try {
 			validateEmail(newEmail);
 		} catch (AddressException e) {
 			error += "Email is invalid. ";
 		}
-		
 
-		
+
+
 		error = error.trim();
 		if (error.length() > 0) {
 			throw new IllegalArgumentException(error);
 		}
-		
-		
+
+
 		user.setPassword(newPassword);
 		user.setFirstName(newFirstName.trim());
 		user.setLastName(newLastName.trim());
 		user.setBirthday(newBirthday);
 		user.setEmail(newEmail);
-		
+
 		userRepository.save(user);
-		
+
 		return user;
 	}
-	
+
 	private static void validateEmail(String email) throws AddressException { // validate that email address is valid format. Throws AddressException if it does not, otherwise, does nothing.
 		InternetAddress address = new InternetAddress(email);
 		address.validate();
 	}
-	
+
 	private static boolean validatePassword(String pass) { // validate password. Returns true if valid, otherwise returns false.
 		boolean valid = true;
 		if (pass == null) {
@@ -158,7 +161,21 @@ public class UserService {
 		}
 		return valid;
 	}
-	
+
+
+	@Transactional
+	public List<User> getAllUsers() {
+		return toList(userRepository.findAll());
+	}
+
+	private <T> List<T> toList(Iterable<T> iterable) {
+		List<T> resultList = new ArrayList<T>();
+		for (T user : iterable) {
+			resultList.add(user);
+		}
+		return resultList;
+	}
+
 	private static boolean validateUsername(String username) { // validate username. Returns true if valid, otherwise returns false.
 		boolean valid = true;
 		if (username == null) {
@@ -166,6 +183,6 @@ public class UserService {
 		} else if (username.length() < 4 || username.length() > 26 || username.contains(" ")) {
 			valid = false;
 		}
-		return valid;		
+		return valid;
 	}
 }
